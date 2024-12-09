fc6141db77dff050d6fccd76ac1da8a708b6f07a
ilanwatech1 <destinyilanwasunday7@gmail.com>
2024-12-09 21:56:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Initialize Swiper
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+  
